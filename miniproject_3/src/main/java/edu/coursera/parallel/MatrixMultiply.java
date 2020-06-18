8f65a7a95f54c1ecd902843027458b47b1a1a6aa
chris <dingmaiqi@gmail.com>
2020-06-18 18:48:17 -0400

    proj3: modified the parallel method using forall2dChunked from PCDP

--- miniproject_3/src/main/java/edu/coursera/parallel/MatrixMultiply.java
+++ miniproject_3/src/main/java/edu/coursera/parallel/MatrixMultiply.java
@@ -1,5 +1,6 @@
 package edu.coursera.parallel;
 
+import static edu.rice.pcdp.PCDP.forall2dChunked;
 import static edu.rice.pcdp.PCDP.forseq2d;
 
 /**
@@ -41,10 +42,10 @@ public final class MatrixMultiply {
     public static void parMatrixMultiply(final double[][] A, final double[][] B,
             final double[][] C, final int N) {
         /*
-         * TODO Parallelize this outermost two-dimension sequential loop to
+         * Parallelize this outermost two-dimension sequential loop to
          * achieve performance improvement.
          */
-        forseq2d(0, N - 1, 0, N - 1, (i, j) -> {
+        forall2dChunked(0, N - 1, 0, N - 1, (i, j) -> {
             C[i][j] = 0.0;
             for (int k = 0; k < N; k++) {
                 C[i][j] += A[i][k] * B[k][j];
